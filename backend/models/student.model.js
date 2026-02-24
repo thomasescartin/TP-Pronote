0c6dad68c17e85e28453fdc5717d73f360deaa36
thomasescartin <thomas.escartin@gmail.com>
2026-02-24 11:03:51 +0100

    modification studien.model.

--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -1,1 +1,9 @@
 import { pool } from "../config/db.js";
+
+export const createStudent = async (name, professorId) => {
+  const [result] = await pool.query(
+    "INSERT INTO studients (name, professor_id) VALUES (?, ?)",
+    [name, professorId]
+  );
+};
+return result.insertId;
